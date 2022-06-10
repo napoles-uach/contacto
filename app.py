1a304190cbee9dd29174f4a82231109acb412bc3
José Manuel Nápoles Duarte <jnapoles@uach.mx>
2022-06-10 15:37:14 -0600

    up

--- app.py
+++ app.py
@@ -8,12 +8,12 @@ outF.write("\n")
 outF.write(crys)
 outF.close()
 uploaded_files = st.sidebar.file_uploader("Choose xyz files", accept_multiple_files=False)
-for uploaded_file in uploaded_files:
-    xyz = uploaded_file.getvalue().decode("utf-8")
-    mol = open("input","w")
-    for line in xyz:
-        mol.write(line,"\n")
-    mol.close()
+#for uploaded_file in uploaded_files:
+#    xyz = uploaded_file.getvalue().decode("utf-8")
+#    mol = open("input","w")
+#    for line in xyz:
+#        mol.write(line,"\n")
+#    mol.close()
 os.system("chmod +x a.out")
 #os.system(" ./a.out")
 #os.system("ls")
