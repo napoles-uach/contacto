9df26edb9d7c81a462c00f02fed47d0ac3f8976b
José Manuel Nápoles Duarte <jnapoles@uach.mx>
2022-06-10 11:29:37 -0600

    code

--- app.py
+++ app.py
@@ -1,3 +1,9 @@
 import streamlit as st
 import os
+outF = open("input","w")
+n=st.text_input("enter n")
+crys = st.text_input("cristal")
+outF.write(n,n,n,"\n")
+outF.write(crys)
+outF.close()
 os.startfile("a.out")
