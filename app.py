a9e0a1e4471d3435369a66a9e425ac5a15131f04
José Manuel Nápoles Duarte <jnapoles@uach.mx>
2022-06-10 15:51:03 -0600

    input

--- app.py
+++ app.py
@@ -1,13 +1,14 @@
 import streamlit as st
 import os
-outF = open("input","w")
+
 n=st.text_input("enter n")
 crys = st.text_input("cristal")
 if (n and crys):
+    outF = open("input","w")
     st.write(n,crys)
-    outF.write(n)
-    outF.write("\n")
-    outF.write(crys)
+    outF.write(n+'\n'+crys)
+    #outF.write("\n")
+    #outF.write(crys)
     outF.close()
 uploaded_file = st.file_uploader("Choose a file")
 if uploaded_file:
