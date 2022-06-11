e25a622f216d1663ff112869b500ef2c792e6d76
José Manuel Nápoles Duarte <jnapoles@uach.mx>
2022-06-10 20:39:32 -0600

    sub

--- app.py
+++ app.py
@@ -3,9 +3,12 @@ import os
 import subprocess
 import sys
 
-
-
+import sys, string, os, subprocess
+from subprocess import PIPE, Popen, STDOUT, STARTUPINFO
 os.system("chmod +x a.out")
+s = Popen("a.out", stdin=PIPE, stdout=PIPE, stderr=PIPE, shell = True)
+
+
 #subprocess.run([f"{sys.executable}", "a.out"])
 subprocess.run(['ls','-l'])
 #os.system("#!/bin/bash a.out")
