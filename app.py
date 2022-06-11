9e55ffcecb87842d5fc6c7947c8a9de640bb3ddc
José Manuel Nápoles Duarte <jnapoles@uach.mx>
2022-06-10 18:11:54 -0600

    file

--- app.py
+++ app.py
@@ -10,22 +10,15 @@ if (n and crys):
     #outF.write("\n")
     #outF.write(crys)
     outF.close()
-uploaded_file = st.file_uploader("Choose a file")
-if uploaded_file:
-#uploaded_files = st.sidebar.file_uploader("Choose xyz files", accept_multiple_files=False)
-#for uploaded_file in uploaded_files:
-    xyz = uploaded_file.getvalue().decode("utf-8")
-    # add a new line when non integer is found in the xyz file
-    #for line in xyz.splitlines():
-    #    st.write(line)
-    
 
-    #st.write(xyz)
-    mol = open("FileInput.xyz","w")
-    for line in xyz.splitlines():
-        st.write(line)
-        mol.write(line+'\n')
-    mol.close()
+#uploaded_file = st.file_uploader("Choose a file")
+#if uploaded_file:
+#
+#    mol = open("FileInput.xyz","w")
+#    for line in xyz.splitlines():
+#        st.write(line)
+#        mol.write(line+'\n')
+#    mol.close()
 os.system("chmod +x a.out")
 os.system("./a.out")
 st.write("done")
