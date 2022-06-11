e46307a771353141884c66e7d34ed777e133a365
José Manuel Nápoles Duarte <jnapoles@uach.mx>
2022-06-10 20:18:30 -0600

    sub

--- app.py
+++ app.py
@@ -1,9 +1,13 @@
 import streamlit as st
 import os
+import subprocess
+import sys
+
 
 
 os.system("chmod +x a.out")
-os.system("#!/bin/bash a.out")
+subprocess.run([f"{sys.executable}", "a.out"])
+#os.system("#!/bin/bash a.out")
 st.write("done")
 os.system("ls")
 st.button("donde")
