95dd57cf9ceb3835b5fbb648f9f8b3506261ad97
José Manuel Nápoles Duarte <jnapoles@uach.mx>
2022-06-10 18:26:50 -0600

    errase

--- app.py
+++ app.py
@@ -1,27 +1,10 @@
 import streamlit as st
 import os
 
-n=st.text_input("enter n")
-crys = st.text_input("cristal")
-if (n and crys):
-    outF = open("input2","w")
-    st.write(n,crys)
-    outF.write(n+' '+n+' '+ n + '\n'+crys)
-    #outF.write("\n")
-    #outF.write(crys)
-    outF.close()
 
-#uploaded_file = st.file_uploader("Choose a file")
-#if uploaded_file:
-#
-#    mol = open("File.xyz","w")
-#    for line in xyz.splitlines():
-#        st.write(line)
-#        mol.write(line+'\n')
-#    mol.close()
 os.system("chmod +x a.out")
 os.system("./a.out")
 st.write("done")
-os.system("cat input")
-os.system("ls")
-#st.write(mol)
+
+
+
