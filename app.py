8294dbad30a7f54cb70f631f37dc010485f84c59
José Manuel Nápoles Duarte <jnapoles@uach.mx>
2022-06-10 21:00:20 -0600

    bas

--- app.py
+++ app.py
@@ -9,9 +9,9 @@ os.system("chmod +x a.out")
 
 
 
-subprocess.run([f"{sys.executable}", "a.out"])
-subprocess.run(['ls','-l'])
-#os.system("#!/bin/bash a.out")
+#subprocess.run([f"{sys.executable}", "a.out"])
+#subprocess.run(['ls','-l'])
+os.system("#!/bin/bash a.out")
 st.write("done")
 
 
